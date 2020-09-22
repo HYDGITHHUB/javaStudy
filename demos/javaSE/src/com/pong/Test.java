ef5cd5e633300e7f0e42699a08d32b6cfef78f54
HYDGITHHUB <2607223301@qq.com>
2020-09-22 21:23:18 +0800

    daily:JUC study

---
+++ demos/javaSE/src/com/pong/Test.java
@@ -0,0 +1,13 @@
+package com.pong;
+
+public class Test {
+    public static void main(String[] args) {
+        char a = 'a';
+
+        System.out.println(a);
+
+        System.out.println((char)((int) a+1));
+
+        System.out.println((char) 98);
+    }
+}
